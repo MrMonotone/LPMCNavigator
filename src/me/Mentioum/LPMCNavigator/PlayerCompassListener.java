8af298674c86fed64327a6c161f892adf1433adb
Mentioum <mentioum@gmail.com>
2011-07-23 04:59:03 +0100

    Created Classes PlayerCompassRecorder and PlayerCompassReader

--- src/me/Mentioum/LPMCNavigator/PlayerCompassListener.java
+++ src/me/Mentioum/LPMCNavigator/PlayerCompassListener.java
@@ -28,6 +28,7 @@ public class PlayerCompassListener  extends PlayerListener{
         String playerworldlocation = player.getLocation().getWorld().getName();
         Arguments entry = new Arguments(player.getDisplayName());
         
+        player.setCompassTarget(playerstoredlocation);
         
         int playerstoredlocationxint = (int)playerstoredlocation.getX();
         int playerstoredlocationyint = (int)playerstoredlocation.getY();
@@ -37,58 +38,54 @@ public class PlayerCompassListener  extends PlayerListener{
         String playerstoredlocationy = Integer.toString(playerstoredlocationyint);
         String playerstoredlocationz = Integer.toString(playerstoredlocationzint);
         
-        if (helditem.getTypeId() == 345 && action == Action.RIGHT_CLICK_AIR || action == Action.RIGHT_CLICK_BLOCK){
+        if (helditem.getTypeId() == 345){
             
-            entry.setValue("x", String.valueOf(playerstoredlocationx) );
-            entry.setValue("y", String.valueOf(playerstoredlocationy) );
-            entry.setValue("z", String.valueOf(playerstoredlocationz) );
-            entry.setValue("world", playerworldlocation);
+            if(action == Action.RIGHT_CLICK_AIR || action == Action.RIGHT_CLICK_BLOCK){
+                    
+                entry.setValue("x", String.valueOf(playerstoredlocationx) );
+                entry.setValue("y", String.valueOf(playerstoredlocationy) );
+                entry.setValue("z", String.valueOf(playerstoredlocationz) );
+                entry.setValue("world", playerworldlocation);
+
+                plugin.database.addIndex(entry.getKey(), entry);
+
+                plugin.database.update();
+
+                String locationstored = ChatColor.GREEN + "Compass tuned to your current location in " + entry.getValue("world") + ":";
+                String locationstoredx = ChatColor.BLUE + "X: " + ChatColor.WHITE + entry.getValue("x");
+                String locationstoredy = ChatColor.BLUE + "Y: " + ChatColor.WHITE + entry.getValue("y");
+                String locationstoredz = ChatColor.BLUE + "Z: " + ChatColor.WHITE + entry.getValue("z");
+                player.sendMessage(locationstored);
+                player.sendMessage(locationstoredx);
+                player.sendMessage(locationstoredy);
+                player.sendMessage(locationstoredz);
+            }
             
-            plugin.database.addIndex(entry.getKey(), entry);
-            
-            plugin.database.update();
-            
-            String locationstored = ChatColor.GREEN + "Compass tuned to your current location in " + entry.getValue("world") + ":";
-            String locationstoredx = ChatColor.BLUE + "X: " + ChatColor.WHITE + entry.getValue("x");
-            String locationstoredy = ChatColor.BLUE + "Y: " + ChatColor.WHITE + entry.getValue("y");
-            String locationstoredz = ChatColor.BLUE + "Z: " + ChatColor.WHITE + entry.getValue("z");
-            player.sendMessage(locationstored);
-            player.sendMessage(locationstoredx);
-            player.sendMessage(locationstoredy);
-            player.sendMessage(locationstoredz);
-           
-    }    
-        else{    
-             if(helditem.getTypeId() == 345 && action == Action.LEFT_CLICK_AIR  || action == Action.LEFT_CLICK_BLOCK){
-                Arguments getentry = plugin.database.getArguments(player.getDisplayName());
+            if(action == Action.LEFT_CLICK_AIR || action == Action.LEFT_CLICK_BLOCK){
+                    Arguments getentry = plugin.database.getArguments(player.getDisplayName());
                         
-                
-                String world = getentry.getValue("world");
+              
                 
                         double x = Double.parseDouble(getentry.getValue("x"));
                         double y = Double.parseDouble(getentry.getValue("y"));
                         double z = Double.parseDouble(getentry.getValue("y"));
                         
-                        Location newLocation = new Location(plugin.getServer().getWorld(world), x, y, z);
                  
-                        player.setCompassTarget(newLocation);
+                        player.setCompassTarget(player.getWorld().getSpawnLocation());
+                        
                         
                         String locationstoredx = ChatColor.BLUE +  "X: " + ChatColor.WHITE + getentry.getValue("x");
                         String locationstoredy = ChatColor.BLUE + "Y: " + ChatColor.WHITE +  getentry.getValue("y");
                         String locationstoredz = ChatColor.BLUE + "Z: " + ChatColor.WHITE + getentry.getValue("z");
                         
-                        player.sendMessage(ChatColor.GREEN + "Compass Pointed to:");
-                        player.sendMessage(locationstoredx);
-                        player.sendMessage(locationstoredy);
-                        player.sendMessage(locationstoredz);
-                        
-        }
-        
-        
-             else{
-                 
+                        player.sendMessage(ChatColor.GREEN + "Compass Pointed to Spawn:");
+                       //ayer.sendMessage(locationstoredx);
+                       //layer.sendMessage(locationstoredy);
+                       //layer.sendMessage(locationstoredz);
+            }
+            else{
                 return;
-                 }
+            }
              
         }
              
